c7f59d2e74c1869269f986526e73ee9438d22e04
Vladyslav Lunov <vladyslavlunov@gmail.com>
2026-01-30 23:41:42 +0100

    feat(security): base config and implementation added

---
+++ src/main/java/dev/lunov/eng/security/TokenService.java
@@ -0,0 +1,36 @@
+package dev.lunov.eng.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final JwtEncoder encoder;
+
+
+    public String generate(Authentication authentication) {
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
